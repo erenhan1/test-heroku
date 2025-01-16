9039bc2825ad941355ef06d4248fae6d8803446b
Eren Han <47918722+erenhan1@users.noreply.github.com>
2025-01-16 14:30:07 +0200

    Update dashtest.py

--- dashtest.py
+++ dashtest.py
@@ -87,4 +87,4 @@ def update_graphs(value):
 
 # Run the app locally
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050))
